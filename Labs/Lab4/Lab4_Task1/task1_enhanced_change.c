6b10e4c1772c4d828136253a07d1d13d845d94bb
chaosinnovator <thomas.boland@outlook.com>
2025-10-07 20:14:03 -0700

    Add Lab4 Task1

---
+++ Labs/Lab4/Lab4_Task1/task1_enhanced_change.c
@@ -0,0 +1,14 @@
+/*
+* 2025F COMP-166 Lab 4 - Thomas Boland - C0556991
+* 
+* This program converts a given amount of money to the optimal
+* quantity of each denomination, drawing from a limited inventory
+* of cash.
+* 
+*/
+
+#include <stdlib.h>
+
+int main() {
+	return EXIT_SUCCESS;
+}
